836ccc713e3763bccbddb8ab25940d8aae4c99c6
GODCHINMAY <cagrawal@udel.edu>
2025-09-07 21:09:46 -0400

    added cutecat pic, solved html css problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
 
 function App(): React.JSX.Element {
@@ -7,6 +8,21 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1 className="heading">
+                This is a header
+            </h1>
+            Hello World
+            Chinmay Agrawal
+            return <div>
+        <Button onClick={ () => { console.log("Hello World!") } }>Log Hello World</Button>
+    </div>;
+            <img src= "../src/assets/cutecat.jpeg" alt="Cute Cat" />
+            <div>Ordered List:</div>
+            <ol style={{ backgroundColor: "red" }}>
+                <li>First thing</li>
+                <li>Second thing</li>
+                <li>A third item</li>
+            </ol>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
